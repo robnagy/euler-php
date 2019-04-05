e71dc852729718fa6e7593597336b14d8efb3496
Robert Nagy <robnagy@users.noreply.github.com>
2019-04-05 01:01:13 +0100

    solution 5

--- app/Services/Solutions/Solution5.php
+++ app/Services/Solutions/Solution5.php
@@ -7,6 +7,13 @@ use App\Helpers\Multiples;
 
 class Solution5 extends BaseSolution implements SolutionInterface
 {
+    /**
+     * Returns the smallest positive number
+     * that is evenly divisible by all
+     * of the numbers from 1 to 20
+     *
+     * @return void
+     */
     public function run()
     {
         $limit = 20;
