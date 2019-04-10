88196e3daff98efad8cb6d30a8f935cfcd7e0f82
Robert Nagy <robnagy@users.noreply.github.com>
2019-04-10 23:29:37 +0100

    solution 1

--- app/Services/Solutions/Solution1.php
+++ app/Services/Solutions/Solution1.php
@@ -9,11 +9,16 @@ use App\Helpers\Multiples;
 
 class Solution1 extends BaseSolution implements SolutionInterface
 {
+    /**
+     * Finds the sum of all the multiples of 3 or 5 below 1000
+     *
+     * @return void
+     */
     public function run()
     {
         $num1 = 3;
         $num2 = 5;
-        $limit = 2000;
+        $limit = 999;
 
         return Multiples::sumOfMultiples($num1, $num2, $limit);
     }
